b01f2407e649985a0871e5fec7ecd6679c8fb014
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-12-21 23:24:14 +0100

    update pagina dettagli

--- resources/views/pages/item_details.blade.php
+++ resources/views/pages/item_details.blade.php
@@ -8,7 +8,26 @@
 
 
     <div class="first_description">
-        <h1>{{ $item["title"] }}</h1>
+        <div class="row">
+            <div class="col-8 description">
+                <h1>{{ $item["title"] }}</h1>
+                <div class="priceANDavailability">
+                    <div class="p-3 d-flex border-bottom border-secondary">
+                        <span class="green_text">U.S. Price: </span><span class="text-white ms-1">{{ $item["price"] }}</span>
+                        <span class="green_text ms-auto pe-4">AVAILABLE</span>
+
+                        <span class="ps-4 text-white border-start border-secondary">Check Availability</span>
+                    </div>
+                    
+
+                </div>
+
+            </div>
+            <div class="col-4 adv">
+                <h4>ADVERTISEMENT</h4>
+                <img src="{{ asset('img/adv.jpg') }}" alt="">
+            </div>
+        </div>
 
     </div>
 
