c4b6cd8db858d7002c20f15174523d8f61fa8726
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-12-21 16:58:59 +0100

    layout homepage (senza particolari di stile)

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,20 +7,22 @@
 
 <div class="bg-dark text-white">
     {{-- @dump($comics) --}}
-    <div class="container py-5 bg-dark">
-        <div class="row">
-            @foreach ($comics as $item)
-                
-            <div class="col">
-
-                <div class="cont_img">
-                <img src="{{ $item['thumb'] }}" alt="" />
-                </div>
-                <p>{{ $item['title'] }}</p>
+    <div class="card_cont_black">
+        <div class="container card_section">
+            <div class="d-flex flex-wrap justify-content-center">
+                @foreach ($comics as $item)
+                    <a class="card_cont" href="#">
+                        <div class="cont_img">
+                            <img src="{{ $item['thumb'] }}" alt="" />
+                        </div>
+                        <p>{{ $item['title'] }}</p>
+                    </a>
+                @endforeach
             </div>
-            @endforeach
+            <div class="cont_btn">
+                <button class="btn_load_more"><span href="#">LOAD MORE</span></button>
+            </div>
         </div>
-
     </div>
 
 </div>
