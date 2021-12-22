d1a766a080f7276124c33b7fdc703d4e7a875261
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-12-22 19:21:47 +0100

    update pagina dettagli

--- resources/views/pages/item_details.blade.php
+++ resources/views/pages/item_details.blade.php
@@ -6,21 +6,23 @@
 
 
 
-
+@dump($item)
+@dump($item["artists"])
     <div class="first_description">
         <div class="row">
             <div class="col-8 description">
                 <h1>{{ $item["title"] }}</h1>
                 <div class="priceANDavailability">
-                    <div class="p-3 d-flex border-bottom border-secondary">
-                        <span class="green_text">U.S. Price: </span><span class="text-white ms-1">{{ $item["price"] }}</span>
+                    <div class=" d-flex border-bottom border-secondary">
+                        <span class="ps-4 green_text">U.S. Price: </span><span class="text-white ms-1">{{ $item["price"] }}</span>
                         <span class="green_text ms-auto pe-4">AVAILABLE</span>
 
-                        <span class="ps-4 text-white border-start border-secondary">Check Availability</span>
+                        <span class="px-4 text-white border-start border-secondary">Check Availability</span>
                     </div>
                     
 
                 </div>
+                <p class="mt-2">{{ $item["description"] }}</p>
 
             </div>
             <div class="col-4 adv">
@@ -31,14 +33,31 @@
 
     </div>
 
-
-
-
+</div>
     <div class="second_description">
+        <div class="details_container row">
+            <div class="col-6 border">
+                <div>
+                    <h3>Talent</h3>
+                    <div class="row">
+                        <div class="col-4"><h5>Art By:</h5></div>
+                        <div class="col-8 artist">
+                            @foreach ($item["artists"] as $artist)
+                                <a>{{ $artist }}, </a>
+                            @endforeach
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 border">{{-- {{ $item["artists"] }} --}}
+                
+            </div>
+            
+        </div>
 
     </div>
 
 
-</div>
     
 @endsection
